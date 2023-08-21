f5247040aa5c906fc334081f911f943d574405a5
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2023-08-21 12:44:13 -0300

    CHANGELOG: Adição de novas rotas (Rota / e Rota para tratamento de erro 404) - Ismael Santiago

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
-app = Flask()
+app = Flask(__name__)
 
+#Rota principal
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+#Rota para tratar erro 404
+@app.errorhandler(404)
+def error(error):
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
