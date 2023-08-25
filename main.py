e768dcaed148bdef085f150bb64b9f997ec2dd1f
Rafael12023 <rafaell.resendee@gmail.com>
2023-08-24 21:47:02 -0300

    Enviado hoje 24-10

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from flask import Flask
 
 #importação da rota "minha_rota", que se encontra na pasta "routes" 
-from routes import minha_rota
+from routes import login
 
 #exemplo de importação para mais de uma rota
 """
@@ -12,8 +12,8 @@ from routes import minha_rota, minha_segunda_rota, minha_terceira_rota
 #instanciando flask
 app = Flask(__name__)
 
-#registrando rota "minha_rota", (ORBIGATORIAMENTE COLOR ".bp" NO FINAL DO ARQUIVO DA ROTA)
-app.register_blueprint(minha_rota.bp) #para registrar mais rotas copie esta linha e altere somente o nome da nova rota
+#registrando rota "/login"
+app.register_blueprint(login.bp) 
 
 #verificando se está em ambiente de desenvolvimento
 if __name__ == "__main__":
