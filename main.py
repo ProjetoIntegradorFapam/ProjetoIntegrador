7848f9b750ea93b4709658859b1b58d510908ae9
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2023-08-25 11:29:55 -0300

    Update 25-10

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #importação de bibliotecas, frameworks e microframeworks
-from flask import Flask
+from flask import Flask, session
 
 #importação da rota "login"
 from routes import login
 
 #instanciando flask
 app = Flask(__name__)
+
+#configurando secret_key
+app.secret_key = "hsjebej337isb"
 
 #registrando rota "/login"
 app.register_blueprint(login.bp)
--- routes/login.py
+++ routes/login.py
@@ -1,11 +1,15 @@
 #importando bibliotecas, frameworks e microframeworks
 #o microframework blueprint é responsável por criar uma coleção de views, ou seja, rotas separadas.
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, flash
 
 #instanciando "login" utilizando Blueprint (É OBRIGRATÓRIO utilizar "__name__" após o nome da view, ou seja, rota.)
 bp = Blueprint('login', __name__)
 
 #Definindo rota "/login"
-@bp.route('/login')
+@bp.route('/login', methods=['GET','POST'])
 def login():
+  if request.method == "GET":
     return render_template('login.html')
+  elif request.method == "POST":
+    flash('Logado com sucesso!')
+    return redirect('/login')
