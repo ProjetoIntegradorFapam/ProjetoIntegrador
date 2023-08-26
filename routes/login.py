e99967303391f267410571c66a2c81524e444123
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2023-08-25 23:28:42 -0300

    Tratamento de dados e personalização login.

--- routes/login.py
+++ routes/login.py
@@ -8,8 +8,32 @@ bp = Blueprint('login', __name__)
 #Definindo rota "/login"
 @bp.route('/login', methods=['GET','POST'])
 def login():
+
+  #Tratamento de rotas
+
+  #Trantando rota "GET" para renderizar o login.html
   if request.method == "GET":
+
     return render_template('login.html')
+  
+  #Tratando rota "POST" para validar os dados e fazer o redirecionamento
   elif request.method == "POST":
-    flash('Logado com sucesso!')
-    return redirect('/login')
+
+    #Dados do Front-End (AVISO: O CPF ESTÁ SENDO FORMATADO PORQUE O FRONT POSSUI UMA MÁSCARA QUE INCLUI CARACTERES NA STRING)
+    cpf = str(request.form.get('cpf')).translate(str.maketrans('', '', '.-'))
+    password = str(request.form.get('password'))
+
+    #Validação dos dados (AVISO: USUÁRIO E SENHA PARA TESTES)
+    if cpf == "00000000000" and password == "12345":
+
+      #Enviando mensagem de sucesso e fazendo o redirecionamento para a rota "/home"
+      flash('Login efetuado com sucesso!', 'bg-sucess')
+      return redirect('/home')
+
+    else:
+
+      #Enviando mensagem de erro e fazendo o redirecionamento para a rota "/login"
+      flash('Falha ao efetuar o login!', 'bg-danger')
+      return redirect('/login')
+  else:
+    return
