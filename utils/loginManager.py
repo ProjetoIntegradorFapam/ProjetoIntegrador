43bfeca3bdcf9c009cbbef01b498f3f93586bb5b
IsmaelSantiagoo <ismaelfreitas.santiago@gmail.com>
2023-10-28 02:21:25 -0300

    Restringindo acesso a variáveis da classe LoginManager

--- utils/loginManager.py
+++ utils/loginManager.py
@@ -2,27 +2,37 @@ class loginManager:
 
     def __init__(self):
 
-        self.authenticated = False
+        self.__authenticated = False
 
+    @property
+    def getAuthentication(self):
+
+        return self.__authenticated
+    
+    @getAuthentication.setter
+    def setAuthentication(self, status):
+
+        self.__authenticated = status
+    
     def authenticate(self):
 
-        self.authenticated = True
-        if self.authenticated == True:
+        self.__authenticated = True
+        if self.__authenticated == True:
             return True
         else:
             return False
     
     def logout(self):
 
-        self.authenticated = False
-        if self.authenticated == False:
+        self.__authenticated = False
+        if self.__authenticated == False:
             return True
         else:
             return False
     
     def isAuthenticated(self):
 
-        if self.authenticated == True:
+        if self.__authenticated == True:
             return True
         else:
             return False
